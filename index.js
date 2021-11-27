6094e1b44a44a3ad86eaa065d404475c2e666ca3
jeng200 <jeng200@yahoo.com>
2021-11-26 20:18:39 -0500

    Create Delete All and Save Tab buttons. Set up local storage.

--- index.js
+++ index.js
@@ -2,24 +2,49 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+const tabs = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+  console.log(tabs[0].url)
+})
+
+
+function render(leads) {
+  let listItems = ""
+  for (let i = 0; i < leads.length; i++) { //use backticks in template strings
+   listItems += ` 
+    <li>
+     <a target='_blank' href='${leads[i]}'> 
+       ${leads[i]}
+     </a>
+    </li>
+   `
+  }
+  ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
 
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value) //push value from input field into myLeads array
   inputEl.value=""
-  renderLeads()
-  
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads) 
 })
-function renderLeads() {
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) { //use backticks in template strings
- listItems += ` 
-  <li>
-   <a target='_blank' href='${myLeads[i]}'> 
-     ${myLeads[i]}
-   </a>
-  </li>
- `
-}
 
-ulEl.innerHTML = listItems
-}
+
+
